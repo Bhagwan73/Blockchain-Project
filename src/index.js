1e6f737a5591f4ff6abf9b6afedd578237bb7b97
Bhagwan73 <bhagwan735025@gmail.com>
2022-12-09 00:11:48 +0530

    small_changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const route = require("./routes/route");
+const route = require("./route");
 const app = express();
 const mongoDB= "mongodb+srv://BhagwanNavthar:sOqsn7dh8KuLiKHp@cluster0.j8ysgx2.mongodb.net/Bhagwan73-DB"
 
+mongoose.set('strictQuery', false)
 mongoose.connect(  mongoDB,{ useNewUrlParser: true },(err)=>{
   if(err){
     console.log("mongoDB is not connected",err)
